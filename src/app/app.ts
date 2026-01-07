c2074f2fc3b5bd7c7480407b3557288ef9afea41
Jasurbek Mamatqulov <mamatqulovjasurbek805@outlook.com>
2026-01-07 19:06:32 +0500

    simple-calculator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MyButton } from './my-button/my-button';
+import { MathOperations } from './math-operations/math-operations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MyButton, MathOperations],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/my-button/my-button.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-button',
+  imports: [],
+  templateUrl: './my-button.html',
+  styleUrl: './my-button.css',
+})
+export class MyButton {
+ 
+  count = 0;
+  decrement() {
+    this.count--;
+  }
+  increment() {
+    this.count++;
+  }
+}
+
